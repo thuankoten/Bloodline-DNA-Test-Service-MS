f3ec5f2acdcc5a9c11fb04f049920938249813de
nguyenliem0507 <nguyenliem9514@gmail.com>
2025-06-21 16:36:45 +0700

    Thêm class BloodlineApplication để khởi chạy Spring Boot

---
+++ src/main/java/com/bloodline/BloodlineApplication.java
@@ -0,0 +1,11 @@
+package com.bloodline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BloodlineApplication.class, args);
+    }
+}
