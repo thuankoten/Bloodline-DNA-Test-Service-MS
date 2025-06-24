a4d725c11c970dfaa57f658138a3eec54ac85f05
nguyenliem0507 <nguyenliem9514@gmail.com>
2025-06-24 22:57:20 +0700

    Thêm file

---
+++ src/main/java/com/bloodline/entity/OrderTest.java
@@ -0,0 +1,88 @@
+package com.bloodline.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users_order_test")
+public class OrderTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(name = "test_type")
+    private String testType;
+
+    @Column(name = "recipient_name")
+    private String recipientName;
+
+    @Column(name = "appointment_date")
+    private String appointmentDate;
+
+    @Column(name = "appointment_time")
+    private String appointmentTime;
+
+    @Column(name = "sample")
+    private String sample;
+
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getSample() {
+        return sample;
+    }
+
+    public void setSample(String sample) {
+        this.sample = sample;
+    }
+
+}
