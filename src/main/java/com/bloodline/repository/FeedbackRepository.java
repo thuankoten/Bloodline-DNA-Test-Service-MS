e76fc94f13e39fc7cdca8e82d1898bf7f4a185eb
Từ Đạt Nhân Đảm <tudatnhandam1219@gmail.com>
2025-06-25 20:04:59 +0700

    Thêm file java entity, controller và repository cho feedback

---
+++ src/main/java/com/bloodline/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.bloodline.repository;
+
+import com.bloodline.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+}
