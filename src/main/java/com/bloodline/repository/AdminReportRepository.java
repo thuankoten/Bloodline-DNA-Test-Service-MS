34840181f793e2ccf5b8fa467af59dc46bd6f316
nguyenliem0507 <jinkun22112005@gmail.com>
2025-06-25 22:29:11 +0700

    add AdminReportRepository

---
+++ src/main/java/com/bloodline/repository/AdminReportRepository.java
@@ -0,0 +1,7 @@
+package com.bloodline.repository;
+
+import com.bloodline.entity.AdminReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminReportRepository extends JpaRepository<AdminReport, Long> {
+}
