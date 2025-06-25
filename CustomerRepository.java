deca9ebbd558644802c9d7522a756da3ad327d23
Trần Thuận <91060466+thuankoten@users.noreply.github.com>
2025-06-25 16:20:50 +0700

    create CustomerRepository.java

---
+++ CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.bloodline.repository;
+
+import com.bloodline.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
