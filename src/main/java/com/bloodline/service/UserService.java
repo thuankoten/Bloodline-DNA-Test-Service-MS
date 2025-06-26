494b69ffd62af03ce911c14886aeff5117e29871
Trần Thuận <91060466+thuankoten@users.noreply.github.com>
2025-06-26 20:02:01 +0700

    Create UserService.java

---
+++ src/main/java/com/bloodline/service/UserService.java
@@ -0,0 +1,31 @@
+package com.bloodline.service;
+
+import com.bloodline.entity.User;
+import com.bloodline.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+}
