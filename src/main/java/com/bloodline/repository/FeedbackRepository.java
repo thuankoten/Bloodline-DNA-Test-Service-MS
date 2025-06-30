c138da1dbbcb6e0250a8f23be274df59375d9456
nguyenliem0507 <nguyenliem9514@gmail.com>
2025-06-30 17:05:47 +0700

    Update

--- src/main/java/com/bloodline/repository/FeedbackRepository.java
+++ src/main/java/com/bloodline/repository/FeedbackRepository.java
@@ -1,10 +1,12 @@
 package com.bloodline.repository;
 
+import java.util.List;
 import com.bloodline.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
+    List<Feedback> findByCustomerName(String customerName);
+    List<Feedback> findByReplyIsNull();
 }
