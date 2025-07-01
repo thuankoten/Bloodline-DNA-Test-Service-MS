36b331526815065685c1090f789ce66b426201c1
nguyenliem0507 <jinkun22112005@gmail.com>
2025-07-01 21:07:37 +0700

    update AdminReprot.java

--- src/main/java/com/bloodline/entity/AdminReport.java
+++ src/main/java/com/bloodline/entity/AdminReport.java
@@ -17,5 +17,43 @@ public class AdminReport {
     private Integer report_value;
 
     // Getters và Setters
-    // ...
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(LocalDate report_date) {
+        this.report_date = report_date;
+    }
+
+    public Integer getReport_value() {
+        return report_value;
+    }
+
+    public void setReport_value(Integer report_value) {
+        this.report_value = report_value;
+    }
 }
