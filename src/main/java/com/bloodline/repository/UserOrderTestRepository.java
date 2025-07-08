fd821e7a4f7f7922ee4aa407f39128067a5fc360
Trần Thuận <91060466+thuankoten@users.noreply.github.com>
2025-07-08 17:56:06 +0700

    Update UserOrderTestRepository.java

--- src/main/java/com/bloodline/repository/UserOrderTestRepository.java
+++ src/main/java/com/bloodline/repository/UserOrderTestRepository.java
@@ -3,5 +3,8 @@ package com.bloodline.repository;
 import com.bloodline.entity.UserOrderTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserOrderTestRepository extends JpaRepository<UserOrderTest, Long> {
+    List<UserOrderTest> findByUsername(String username);
 }
