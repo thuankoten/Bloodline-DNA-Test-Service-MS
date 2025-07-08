84d3ee058b660e0ff68fb6eaeb89644a21ec30fa
Trần Thuận <91060466+thuankoten@users.noreply.github.com>
2025-07-08 17:56:59 +0700

    Update BloodlineApplication.java

--- src/main/java/com/bloodline/BloodlineApplication.java
+++ src/main/java/com/bloodline/BloodlineApplication.java
@@ -1,11 +1,25 @@
 package com.bloodline;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class BloodlineApplication {
     public static void main(String[] args) {
         SpringApplication.run(BloodlineApplication.class, args);
+    }
+
+    @Configuration
+    public static class JacksonConfig {
+        @Bean
+        public ObjectMapper objectMapper() {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            return mapper;
+        }
     }
 }
