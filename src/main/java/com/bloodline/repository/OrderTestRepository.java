9a7ec07eb133df976d84cf4d417aad946ff005d8
HuynhhBao <hbao70018@gmail.com>
2025-07-08 17:08:06 +0700

    OrderTestRepository.java

--- src/main/java/com/bloodline/repository/OrderTestRepository.java
+++ src/main/java/com/bloodline/repository/OrderTestRepository.java
@@ -2,8 +2,12 @@ package com.bloodline.repository;
 
 import com.bloodline.entity.OrderTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OrderTestRepository extends JpaRepository<OrderTest, Long> {
     List<OrderTest> findByUsername(String username);
+
+    @Query("SELECT o.status, COUNT(o) FROM OrderTest o GROUP BY o.status")
+    List<Object[]> countByStatus();
 }
